0fd007565c74d26f2fe9bb3870af2ee98667d293
Jan Schoelch <jan.schoelch@web.de>
2016-09-13 18:27:48 +0200

    initial master commit server

---
+++ server/src/main/java/de/hska/Ibsys2Application.java
@@ -0,0 +1,12 @@
+package de.hska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ibsys2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ibsys2Application.class, args);
+	}
+}
