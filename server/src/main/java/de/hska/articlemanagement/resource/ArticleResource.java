d1d9d9bea6a96509a10741476a648754adc2ff0c
andreas.foitzik <andreas.foitzik@eviarc.com>
2016-10-26 21:11:06 +0200

    article REST setup

---
+++ server/src/main/java/de/hska/articlemanagement/resource/ArticleResource.java
@@ -0,0 +1,46 @@
+package de.hska.articlemanagement.resource;
+
+import de.hska.articlemanagement.business.IArticleRepository;
+import de.hska.articlemanagement.domain.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * Created by andre on 26.10.2016.
+ */
+@RestController(value = "Article Resource")
+@RequestMapping(value = "/api/rest/articles", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
+public class ArticleResource {
+
+    @Autowired
+    private IArticleRepository articleRepository;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(method = RequestMethod.POST)
+    public Article save(@RequestBody @Valid final Article article) {
+        return articleRepository.save(article);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public Article getById(@PathVariable final Long id) {
+        return articleRepository.findOne(id);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public List<Article> getAll() {
+        return articleRepository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public void delete(@PathVariable final Long id) {
+        articleRepository.delete(id);
+    }
+}
