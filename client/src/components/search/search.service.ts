565948c98683ebd99e698a3da3d34cc0742cf3de
PKief <philipp.kief@gmx.de>
2016-10-30 18:23:19 +0100

    translation for search

--- client/src/components/search/search.service.ts
+++ client/src/components/search/search.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { TranslationService } from './../translate/translate.service';
 
 interface SearchEntry {
     id: number;
@@ -10,8 +11,11 @@ interface SearchEntry {
 
 @Injectable()
 export class SearchService {
+
+    constructor(private translationService: TranslationService){}
+
     // the search results are routes on which you can navigate
-    private searchEntries: SearchEntry[] = [
+    private searchEntriesEN: SearchEntry[] = [
         {
             id: 1,
             name: 'Dashboard',
@@ -61,14 +65,67 @@ export class SearchService {
             route: ['/app/planning/material'],
             description: 'Go to the production planning'
         },
-    ]
+    ];
+
+    private searchEntriesDE: SearchEntry[] = [
+        {
+            id: 1,
+            name: 'Dashboard',
+            route: ['/app/dashboard'],
+            tags: ['Start', 'Home', 'Dashboard'],
+            description: 'Statistiken zur Planung anzeigen'
+        },
+        {
+            id: 2,
+            name: 'Lager',
+            tags: ['Lager'],
+            route: ['/app/warehouse'],
+            description: 'Lagerhaus Übersichtsseite',
+        },
+        {
+            id: 3,
+            name: 'Profile',
+            tags: ['Profile', 'Benutzer', 'Konto'],
+            route: ['/app/profile'],
+            description: 'Profileinstellungen'
+        },
+        {
+            id: 4,
+            name: 'Einstellungen',
+            tags: ['Einstellungen'],
+            route: ['/app/settings'],
+            description: 'Verwalten der Einstellungen'
+        },
+        {
+            id: 5,
+            name: 'Planung',
+            tags: ['planung', 'übersicht'],
+            route: ['/app/planning'],
+            description: 'Planung durchführen'
+        },
+        {
+            id: 6,
+            name: 'Materialplanung',
+            tags: ['Materialplanung', 'planung'],
+            route: ['/app/planning/material'],
+            description: 'Materialplanung durchführen'
+        },
+        {
+            id: 7,
+            name: 'Produktionsplanung',
+            tags: ['Produktionsplanung', 'planung'],
+            route: ['/app/planning/production'],
+            description: 'Produktionsplanung durchführen'
+        },
+    ];
 
     getResults(term: string, amount?: number): Array<any> {
         let results = [];
         if (term.trim().length === 0) {
             return results;
         } else {
-            this.searchEntries.forEach(entry => {
+            let searchEntries = this.translationService.currentLanguage==='de'?this.searchEntriesDE:this.searchEntriesEN;
+            searchEntries.forEach(entry => {
                 if ((entry.name.toLowerCase().indexOf(term.trim().toLowerCase())===0 ||
                     entry.tags.filter(e => e.toLowerCase().indexOf(term.trim().toLowerCase())===0).length > 0) &&
                     results.length <= (amount===undefined?Number.MAX_SAFE_INTEGER:amount)) {
