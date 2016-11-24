cf8c2910a2a344187ce5194c8cb06656e2b0a0e1
PKief <philipp.kief@gmx.de>
2016-11-24 19:45:15 +0100

    renamed component

---
+++ client/src/components/planning_purchasing/planning_purchasing.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'planning_purchasing',
+    templateUrl: './planning_purchasing.component.html',
+    styleUrls: ['./planning_purchasing.component.scss']    
+})
+export class PlanningPurchasingComponent{}
--- client/src/components/purchasing/purchasing.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'purchasing',
-    templateUrl: './purchasing.component.html',
-    styleUrls: ['./purchasing.component.scss']    
-})
-export class PurchasingComponent{}
