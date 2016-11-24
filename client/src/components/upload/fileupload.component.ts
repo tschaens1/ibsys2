8078dda11d657c704efe625fc079343221e45318
PKief <philipp.kief@gmx.de>
2016-11-24 21:16:29 +0100

    updated upload component

--- client/src/components/upload/fileupload.component.ts
+++ client/src/components/upload/fileupload.component.ts
@@ -1,22 +1,29 @@
-import { Component, ElementRef } from '@angular/core';
-import { Http } from '@angular/http';
+import { Component, ElementRef, Inject } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 @Component({
     selector: 'file-upload',
     template: '<input type="file">'
 })
 export class FileUploadComponent {
-    constructor(private http: Http, private el: ElementRef) {}
+
+    uploadUrl: string;
+
+    constructor(private http: Http, private el: ElementRef, @Inject('ApiEndpoint') private apiEndpoint) {
+        this.uploadUrl = apiEndpoint + '/api/rest/uploadXML';
+    }
 
     upload() {
         console.info('send data to server');
         let inputEl = this.el.nativeElement.firstElementChild;
         if (inputEl.files.length > 0) { // a file was selected
-            let file:FileList = inputEl.files[0];
+            let file: FileList = inputEl.files[0];
+            let headers = new Headers({ 'Content-Type': 'application/xml' });
             this.http
-                .post('http://your.upload.url', file)
-                // do whatever you do...
-                // subscribe to observable to listen for response
+                .post(this.uploadUrl, file, { headers: headers })
+                .toPromise()
+                .then(() => alert('ok'))
+                .catch(() => alert('not ok'));
         }
     }
 }
