38f6c6c5158aa5f7c8ff4ef8061683be70ae37a8
Sebastian Dittrich <Sebastian Dittrich>
2016-11-24 23:02:54 +0100

    Planning inserts

---
+++ client/src/components/planning_inserts/planning_inserts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'inserts',
+    templateUrl: './planning_inserts.component.html',
+    styleUrls: ['./planning_inserts.component.scss'] 
+})
+export class PlanningInsertsComponent{     
+    constructor(){
+    }
+}
