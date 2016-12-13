6f9e2de659832cb058675950935d6ac456ee6a16
PKief <philipp.kief@gmx.de>
2016-12-13 21:22:41 +0100

    updated planning preparation

--- client/src/components/upload/fileupload.component.ts
+++ client/src/components/upload/fileupload.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Inject } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { PlanningService } from './../planning/planning.service';
 
 @Component({
     selector: 'file-upload',
@@ -11,7 +12,12 @@ export class FileUploadComponent {
     xmlObject: string;
 
     constructor(
-        private http: Http, private el: ElementRef, @Inject('ApiEndpoint') private apiEndpoint, private toastr: ToastsManager) {
+        private http: Http,
+        private el: ElementRef,
+        @Inject('ApiEndpoint') private apiEndpoint,
+        private toastr: ToastsManager,
+        private planningService: PlanningService,
+    ) {
         this.uploadUrl = apiEndpoint + '/api/rest/file/xml';
     }
 
@@ -21,14 +27,14 @@ export class FileUploadComponent {
             let file: File = inputEl.files[0];
             var myReader: FileReader = new FileReader();
 
-            if(file.type !== 'text/xml'){
+            if (file.type !== 'text/xml') {
                 this.toastr.error('This is not an XML!')
                 return;
             }
 
             myReader.onloadend = (e) => {
-                this.xmlObject = myReader.result;                
-                this.sendToServer(myReader.result);
+                this.xmlObject = myReader.result;
+                this.planningService.xmlDocument = myReader.result;
             }
             myReader.readAsText(file);
         }
