54d64ef33e72ee3b5de26ebe6bb93f3b6c49852f
PKief <philipp.kief@gmx.de>
2016-12-15 21:50:46 +0100

    minor improvements

--- client/src/components/planning/planning.service.ts
+++ client/src/components/planning/planning.service.ts
@@ -35,7 +35,7 @@ export class PlanningService implements OnInit {
      */
     startPlanning(json: any): Promise<any> {
         this.inputJSON = json;
-        this.startedPlanning = true;
+        this.isLoading = true;
         if (this.xmlDocument === undefined) {
             throw new Error('XML is undefined!');
         }
@@ -50,30 +50,25 @@ export class PlanningService implements OnInit {
         this.inputUploadUrl = this.apiEndpoint + `/api/rest/games/${1}/groups/${6}/periods/${this.inputJSON.results.period}/plannings`;
 
         // send data to the server
-        return new Promise((resolve, reject) => {
-            this.sendXMLToServer().then(() => {
-                this.sendInputsToServer().then(() => {
-                    console.log('Send data to server successfully!');
-                });
-            }).catch((err) => reject(err));
-        })
+        return this.sendXMLToServer().then(() => {
+            this.sendInputsToServer();
+        });
     }
-
     /**
      * Send the input data to server. The input data is required
      * for the calculations on the server.
      */
     private sendInputsToServer() {
-        this.isLoading = true;
-
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let body = JSON.stringify(this.inputJSON);
-        // console.log(JSON.stringify(this.inputJSON));
-        alert(this.inputUploadUrl);
         return this.http.post(this.inputUploadUrl, body, { headers: headers })
             .toPromise()
-            .then(() => console.log('Input data successfully uploaded to server as JSON'))
-            .catch(err => console.error('Could not upload input data as JSON to server', err));
+            .then((response) => {
+                console.log('Input data successfully uploaded to server as JSON', response)
+            })
+            .catch((err) => {
+                console.error('Could not upload input data as JSON to server', err);
+            });
     }
 
     /**
@@ -82,11 +77,14 @@ export class PlanningService implements OnInit {
     private sendXMLToServer() {
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let body = JSON.stringify({ content: encodeURIComponent(this.xmlDocument) });
-        alert(this.xmlUploadUrl);
         return this.http.post(this.xmlUploadUrl, body, { headers: headers })
             .toPromise()
-            .then(() => console.log('XML successfully uploaded to server'))
-            .catch(err => console.error('Could not upload XML to server', err));
+            .then((response) => {
+                console.log('XML successfully uploaded to server')
+            })
+            .catch((err) => {
+                console.error('Could not upload XML to server', err)
+            });
     }
 
 }
