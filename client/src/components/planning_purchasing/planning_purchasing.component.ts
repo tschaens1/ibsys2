811e29ed81501f5f7a37f05753779ba91471ae4c
PKief <philipp.kief@gmx.de>
2016-12-26 13:26:27 +0100

    updated generated xml

--- client/src/components/planning_purchasing/planning_purchasing.component.ts
+++ client/src/components/planning_purchasing/planning_purchasing.component.ts
@@ -6,17 +6,6 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './planning_purchasing.component.html',
     styleUrls: ['./planning_purchasing.component.scss']
 })
-export class PlanningPurchasingComponent implements OnInit {
-    resultsFromServer: any;
-    constructor(private planningService: PlanningService) { }
-
-    ngOnInit(): void {
-        this.planningService.resultJSON.subscribe(results => {
-            this.resultsFromServer = results;
-        });
-    }
-
-    onChange(input: HTMLInputElement){
-        
-    }
+export class PlanningPurchasingComponent {    
+    constructor(private planningService: PlanningService) { }    
 }
