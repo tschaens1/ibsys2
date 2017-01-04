820aaaf0e408f13a0305605f4aaf2564411cf513
Philipp Kief <philipp.kief@gmx.de>
2017-01-04 12:45:07 +0100

    Add articles on top

--- client/src/components/planning_material/planning_material.component.ts
+++ client/src/components/planning_material/planning_material.component.ts
@@ -61,7 +61,7 @@ export class PlanningMaterialComponent implements OnInit {
             this.toastr.error(this.translationService.instant('planning_material.validation.invalid_article_id'));
             return;
         }
-        this.planningService.inputDataForSimulatorAsJSON.productionlist.production.push(
+        this.planningService.inputDataForSimulatorAsJSON.productionlist.production.unshift(
             {
                 "@": {
                     "article": articleId,
