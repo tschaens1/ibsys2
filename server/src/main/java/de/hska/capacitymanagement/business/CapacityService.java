52879cfb1d7cf629e60bbc82597972d704b37386
Andreas Foitzik <andreas.foitzik@eviarc.com>
2017-01-10 17:23:07 +0100

    push it to the limit

--- server/src/main/java/de/hska/capacitymanagement/business/CapacityService.java
+++ server/src/main/java/de/hska/capacitymanagement/business/CapacityService.java
@@ -1,8 +1,13 @@
 package de.hska.capacitymanagement.business;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import de.hska.dispositionmanagement.business.DispositionService;
+import de.hska.dispositionmanagement.domain.Disposition;
+import de.hska.partsmanagement.business.PartsService;
 import de.hska.productionmanagement.business.ProductionService;
 import de.hska.workplacemanagement.business.WorkplaceService;
 import de.hska.workplacemanagement.domain.ProductionLine;
@@ -18,36 +23,114 @@ public class CapacityService {
 	@Autowired
 	private ProductionService productionService;
 
+	@Autowired
+	private DispositionService dispositionService;
+
+	@Autowired
+	private PartsService partsService;
+
 	public void initialize() {
 		this.deployExistingCapacities();
 		this.getCalculation();
 	}
 
 	public void deployExistingCapacities() {
-		for (ProductionOrder productionOrderInWaitinglist : this.productionService.getProductionOrdersInWaitinglist()) {
+		ArrayList<ProductionOrder> tmpMergedContent = new ArrayList<ProductionOrder>();
+
+		for (ProductionOrder productionOrderInWaitinglist : productionService.getProductionOrdersInWaitinglist()) {
 			for (ProductionOrder orderInWork : this.productionService.getProductionOrdersInWork()) {
 				ProductionOrder order = new ProductionOrder();
 				if (orderInWork.getOrderId() == productionOrderInWaitinglist.getOrderId()) {
 					order.setInWork(true);
+					order.setOrderId(orderInWork.getOrderId());
 					order.setAmount(productionOrderInWaitinglist.getAmount() + orderInWork.getAmount());
-				} else {
-					order.setInWork(false);
-					order.setAmount(productionOrderInWaitinglist.getAmount());
+					order.setPeriod(this.productionService.getPeriod());
+					order.setProductNumber(productionOrderInWaitinglist.getProductNumber());
+					order.setWorkplaceId(productionOrderInWaitinglist.getWorkplaceId());
+					tmpMergedContent.add(order);
 				}
+			}
+		}
+
+		for (ProductionOrder productionOrderInWaitinglist : productionService.getProductionOrdersInWaitinglist()) {
+			boolean contains = false;
+			for (ProductionOrder tmpOrder : tmpMergedContent) {
+				if (tmpOrder.getOrderId() == productionOrderInWaitinglist.getOrderId()) {
+					contains = true;
+					break;
+				}
+			}
+			if (!contains) {
+				ProductionOrder order = new ProductionOrder();
+				order.setInWork(false);
+				order.setOrderId(productionOrderInWaitinglist.getOrderId());
+				order.setAmount(productionOrderInWaitinglist.getAmount());
 				order.setPeriod(this.productionService.getPeriod());
 				order.setProductNumber(productionOrderInWaitinglist.getProductNumber());
 				order.setWorkplaceId(productionOrderInWaitinglist.getWorkplaceId());
-
-				final ProductionLine productionLine = this.workplaceService.getProductionLineMap()
-						.get(productionOrderInWaitinglist.getProductNumber());
-				productionLine.addProductionOrder(order);
+				tmpMergedContent.add(order);
 			}
+		}
+
+		for (ProductionOrder productionOrderInWork : productionService.getProductionOrdersInWork()) {
+			boolean contains = false;
+			for (ProductionOrder tmpOrder : tmpMergedContent) {
+				if (tmpOrder.getOrderId() == productionOrderInWork.getOrderId()) {
+					contains = true;
+					break;
+				}
+			}
+			if (!contains) {
+				ProductionOrder order = new ProductionOrder();
+				order.setInWork(true);
+				order.setOrderId(productionOrderInWork.getOrderId());
+				order.setAmount(productionOrderInWork.getAmount());
+				order.setPeriod(this.productionService.getPeriod());
+				order.setProductNumber(productionOrderInWork.getProductNumber());
+				order.setWorkplaceId(productionOrderInWork.getWorkplaceId());
+				tmpMergedContent.add(order);
+			}
+		}
+
+		ArrayList<ProductionOrder> dispositionOrder = new ArrayList<ProductionOrder>();
+		for (Disposition disposition : dispositionService.getDisposition()) {
+			if (this.partsService.getManufacturingPartById(disposition.getProduction().getProductNumber())
+					.getUsedInAllProducts()) {
+				boolean contains = false;
+				for (ProductionOrder order : dispositionOrder) {
+					if (disposition.getProduction().getProductNumber() == order.getProductNumber())
+						contains = true;
+				}
+				if (!contains)
+					dispositionOrder.add(disposition.getProduction());
+
+				for (ProductionOrder order : dispositionOrder) {
+					if (disposition.getProduction().getProductNumber() == order.getProductNumber()) {
+						if (contains)
+							order.setAmount(order.getAmount() + disposition.getProduction().getAmount());
+						else
+							order.setAmount(disposition.getProduction().getAmount());
+					}
+				}
+			} else {
+				dispositionOrder.add(disposition.getProduction());
+			}
+		}
+
+		tmpMergedContent.addAll(dispositionOrder);
+
+		System.out.println("Production orders --------------> <-------------");
+		for (ProductionOrder order : tmpMergedContent) {
+			System.out.println(order.toString());
+			final ProductionLine productionLine = this.workplaceService.getProductionLineMap()
+					.get(order.getProductNumber());
+			productionLine.addProductionOrder(order);
 		}
 	}
 
 	public void getCalculation() {
 		for (Workplace workplace : this.workplaceService.getAllWorkPlaces()) {
-			System.out.println("Workplace: " + workplace.getNumber() + " ------------------------");
+			System.out.println("Workplace: " + workplace.getName() + " ------------------------");
 			System.out.println(" -- Working Time: " + workplace.getWorkingTime());
 		}
 	}
