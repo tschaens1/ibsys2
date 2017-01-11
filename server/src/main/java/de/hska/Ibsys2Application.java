454dca061492ce5d37687b40e92819bbe0f2ec57
Andreas Foitzik <andreas.foitzik@eviarc.com>
2017-01-11 19:07:37 +0100

    h2

--- server/src/main/java/de/hska/Ibsys2Application.java
+++ server/src/main/java/de/hska/Ibsys2Application.java
@@ -1,8 +1,11 @@
 package de.hska;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -12,5 +15,12 @@ public class Ibsys2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ibsys2Application.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
 	}
 }
