2ce47637a7abcb8b8822843bfb7de50464e6d930
Jan Schoelch <jan.schoelch@web.de>
2017-01-12 15:28:27 +0100

    Sort capacitites by station, only fill positive values in production and procurement

--- server/src/main/java/de/hska/capacitymanagement/business/CapacityService.java
+++ server/src/main/java/de/hska/capacitymanagement/business/CapacityService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Objects;
 
 @Service
@@ -166,6 +167,7 @@ public class CapacityService {
         capacity.setOvertime(overtime);
 
         this.capacities.add(capacity);
+        this.capacities.sort(Comparator.comparing(Capacity::getStation));
     }
 
     public ArrayList<Capacity> getCapacities() {
--- server/src/main/java/de/hska/inputmanagement/business/InputService.java
+++ server/src/main/java/de/hska/inputmanagement/business/InputService.java
@@ -17,82 +17,84 @@ import org.springframework.stereotype.Service;
 @Service
 public class InputService {
 
-	@Autowired
-	private DispositionService dispositionService;
+    @Autowired
+    private DispositionService dispositionService;
 
-	@Autowired
-	private PlanningService planningService;
+    @Autowired
+    private PlanningService planningService;
 
-	@Autowired
-	private ProcurementService procurementService;
+    @Autowired
+    private ProcurementService procurementService;
 
-	@Autowired
-	private CapacityService capacityService;
+    @Autowired
+    private CapacityService capacityService;
 
-	public JsonFile generateInputJson() {
+    public JsonFile generateInputJson() {
 
-		JSONObject inputJson = new JSONObject();
+        JSONObject inputJson = new JSONObject();
 
-		final JSONObject qualitycontrolItem = new JSONObject();
-		qualitycontrolItem.put("type", "no");
-		qualitycontrolItem.put("losequantity", 0);
-		qualitycontrolItem.put("delay", 0);
+        final JSONObject qualitycontrolItem = new JSONObject();
+        qualitycontrolItem.put("type", "no");
+        qualitycontrolItem.put("losequantity", 0);
+        qualitycontrolItem.put("delay", 0);
 
-		JSONArray sellwishItemList = new JSONArray();
-		for (PlanningPosition planningPosition : this.planningService.getSellwishItems()) {
-			JSONObject sellwishItem = new JSONObject();
-			sellwishItem.put("article", planningPosition.getArticle());
-			sellwishItem.put("quantity", planningPosition.getQuantity());
-			sellwishItemList.put(sellwishItem);
-		}
+        JSONArray sellwishItemList = new JSONArray();
+        for (PlanningPosition planningPosition : this.planningService.getSellwishItems()) {
+            JSONObject sellwishItem = new JSONObject();
+            sellwishItem.put("article", planningPosition.getArticle());
+            sellwishItem.put("quantity", planningPosition.getQuantity());
+            sellwishItemList.put(sellwishItem);
+        }
 
-		JSONArray selldirectItemList = new JSONArray();
-		for (PlanningPosition planningPosition : this.planningService.getSelldirectItems()) {
-			JSONObject selldirectItem = new JSONObject();
-			selldirectItem.put("article", planningPosition.getArticle());
-			selldirectItem.put("quantity", planningPosition.getQuantity());
-			if (planningPosition.getQuantity() > 0)
-				selldirectItemList.put(selldirectItem);
-		}
+        JSONArray selldirectItemList = new JSONArray();
+        for (PlanningPosition planningPosition : this.planningService.getSelldirectItems()) {
+            JSONObject selldirectItem = new JSONObject();
+            selldirectItem.put("article", planningPosition.getArticle());
+            selldirectItem.put("quantity", planningPosition.getQuantity());
+            if (planningPosition.getQuantity() > 0)
+                selldirectItemList.put(selldirectItem);
+        }
 
-		JSONArray productionItemList = new JSONArray();
-		for (Disposition disposition : dispositionService.getDispositions()) {
-			JSONObject productionItem = new JSONObject();
-			productionItem.put("article", disposition.getPartNumber());
-			productionItem.put("quantity", disposition.getFinalNewProduction().getAmount());
-			productionItemList.put(productionItem);
-		}
+        JSONArray productionItemList = new JSONArray();
+        for (Disposition disposition : dispositionService.getDispositions()) {
+            JSONObject productionItem = new JSONObject();
+            productionItem.put("article", disposition.getPartNumber());
+            productionItem.put("quantity", disposition.getFinalNewProduction().getAmount());
+            if (disposition.getFinalNewProduction().getAmount() > 0)
+                productionItemList.put(productionItem);
+        }
 
-		JSONArray orderItemList = new JSONArray();
-		for (BuyOrder buyOrder : procurementService.getNewBuyOrders()) {
-			JSONObject orderItem = new JSONObject();
-			orderItem.put("article", buyOrder.getBuyPartId());
-			orderItem.put("quantity", buyOrder.getAmount());
-			orderItem.put("modus", buyOrder.getBuyMode().getValue());
-			orderItemList.put(orderItem);
-		}
+        JSONArray orderItemList = new JSONArray();
+        for (BuyOrder buyOrder : procurementService.getNewBuyOrders()) {
+            JSONObject orderItem = new JSONObject();
+            orderItem.put("article", buyOrder.getBuyPartId());
+            orderItem.put("quantity", buyOrder.getAmount());
+            orderItem.put("modus", buyOrder.getBuyMode().getValue());
+            if (buyOrder.getAmount() > 0)
+                orderItemList.put(orderItem);
+        }
 
-		JSONArray capacityItemList = new JSONArray();
-		for (Capacity capacity : capacityService.getCapacities()) {
-			JSONObject capacityItem = new JSONObject();
-			capacityItem.put("station", capacity.getStation());
-			capacityItem.put("shift", capacity.getShift());
-			capacityItem.put("overtime", capacity.getOvertime());
-			capacityItemList.put(capacityItem);
-		}
+        JSONArray capacityItemList = new JSONArray();
+        for (Capacity capacity : capacityService.getCapacities()) {
+            JSONObject capacityItem = new JSONObject();
+            capacityItem.put("station", capacity.getStation());
+            capacityItem.put("shift", capacity.getShift());
+            capacityItem.put("overtime", capacity.getOvertime());
+            capacityItemList.put(capacityItem);
+        }
 
-		inputJson.put("qualitycontrol", qualitycontrolItem);
-		inputJson.put("sellwish", sellwishItemList);
-		inputJson.put("selldirect", selldirectItemList);
-		inputJson.put("orderlist", orderItemList);
-		inputJson.put("productionlist", productionItemList);
-		inputJson.put("workingtimelist", capacityItemList);
+        inputJson.put("qualitycontrol", qualitycontrolItem);
+        inputJson.put("sellwish", sellwishItemList);
+        inputJson.put("selldirect", selldirectItemList);
+        inputJson.put("orderlist", orderItemList);
+        inputJson.put("productionlist", productionItemList);
+        inputJson.put("workingtimelist", capacityItemList);
 
-		JSONObject inputItem = new JSONObject();
-		inputItem.put("input", inputJson);
+        JSONObject inputItem = new JSONObject();
+        inputItem.put("input", inputJson);
 
-		JsonFile inputFile = new JsonFile();
-		inputFile.setContent(inputItem.toString());
-		return inputFile;
-	}
+        JsonFile inputFile = new JsonFile();
+        inputFile.setContent(inputItem.toString());
+        return inputFile;
+    }
 }
